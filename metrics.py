8bddbc95a379923e609b82f5d418f7a09b8ce56f
rectorkipa <90633896+rectorkipa@users.noreply.github.com>
2023-04-14 08:43:17 +0300

    Nine homework files of lesson 8 added.

---
+++ metrics.py
@@ -0,0 +1,85 @@
+import numpy as np
+
+
+def precision(recommended_list, bought_list):
+    bought_list = np.array(bought_list)
+    recommended_list = np.array(recommended_list)
+
+    flags = np.isin(bought_list, recommended_list)
+
+    precision = flags.sum() / len(recommended_list)
+
+    return precision
+
+
+def precision_at_k(recommended_list, bought_list, k=5):
+    bought_list = np.array(bought_list)
+    recommended_list = np.array(recommended_list)
+    
+
+    bought_list = bought_list  # Тут нет [:k] !!
+    
+    if k < len(recommended_list):
+        recommended_list = recommended_list[:k]
+
+    flags = np.isin(bought_list, recommended_list)
+
+    precision = flags.sum() / len(recommended_list)
+
+    return precision
+
+
+def money_precision_at_k(recommended_list, bought_list, df_price, k=5):
+    prices_recommended = []
+
+    bought_list = np.array(bought_list)
+    recommended_list = np.array(recommended_list[:k])
+
+    [prices_recommended.append(df_price.loc[df_price['item_id'] == item]['price'].values) for item in
+     recommended_list]
+
+    prices_recommended = np.array(prices_recommended[:k])
+    flags = np.isin(recommended_list, bought_list)
+    prices = flags @ prices_recommended
+    precision = prices.sum() / prices_recommended.sum()
+
+    return float("{0:.4f}".format(precision))
+
+
+def recall(recommended_list, bought_list):
+    bought_list = np.array(bought_list)
+    recommended_list = np.array(recommended_list)
+
+    flags = np.isin(bought_list, recommended_list)
+
+    recall = flags.sum() / len(bought_list)
+
+    return recall
+
+
+def recall_at_k(recommended_list, bought_list, k=5):
+
+    bought_list = np.array(bought_list)
+    recommended_list = np.array(recommended_list)
+
+    if k < len(recommended_list):
+        recommended_list = recommended_list[:k]
+
+    flags = np.isin(bought_list, recommended_list)
+    recall = flags.sum() / len(bought_list)
+
+    return recall
+
+
+def money_recall_at_k(recommended_list, bought_list, prices_recommended, prices_bought, k=5):
+
+    bought_list = np.array(bought_list)
+    prices_bought = np.array(prices_bought)
+    recommended_list = np.array(recommended_list[:k])
+    prices_recommended = np.array(prices_recommended[:k])
+
+    check_bought = np.isin(recommended_list, bought_list) * prices_recommended
+
+    recall = check_bought.sum() / prices_bought.sum()
+
+    return recall
